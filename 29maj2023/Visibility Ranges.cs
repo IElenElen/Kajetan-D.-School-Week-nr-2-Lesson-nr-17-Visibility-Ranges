dd9c2e8401db8b300cb974902c7d6b46744b1e76
Ilka <Ilka@LAPTOP-VAVOB6QC>
2023-05-29 17:44:05 +0200

    Add project files.

---
+++ 29maj2023/Visibility Ranges.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _29maj2023
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Week nr 2 lesson nr 17")
+            Console.WriteLine("Visibility Ranges");
+
+            public int = D;
+        }
+        
+    }
+}
---
+++ Lesson nr 17 próba widoczności/Visibility R 2 Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lesson_nr_17_próba_widoczności
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Visibility Internal Protected Private");
+
+            internal int A;
+
+            protected int B;
+
+            private int C;
+
+            public void Method()
+        {
+            C = 8;
+        }
+
+        }
+    }
+}
